3766123d01e72b7455c511ad9aa3d05dfb683fad
ajith <ajith24ram@gmail.com>
2023-05-27 23:08:25 +0530

    modified nav

--- src/App.js
+++ src/App.js
@@ -1,27 +1,34 @@
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 import './App.css';
 import MyNav from './components/Navbar';
 import WritingsComponent from './components/Writing';
 import Experience from './components/Experience';
 import PortfolioSection from './components/Portfolio';
 import WritingDetailsComponent from './components/Cardpage';
-import ChatWidget from "./components/Chat.js"
-import React, { useState } from 'react';
-import writingsData from './writings_ajith.json';
-import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+import ChatWidget from './components/Chat.js';
 
 const writings = writingsData;
 
 const App = () => {
   const [currentPage, setCurrentPage] = useState('home');
 
+  useEffect(() => {
+    const scrollOptions = {
+      behavior: 'smooth',
+      block: 'start',
+    };
+    window.scrollTo(scrollOptions);
+  }, [currentPage]);
+
   const openNav = () => {
-    document.getElementById("myNav").style.width = "100%";
-    document.getElementById("myNav").style.height = "100%";
+    document.getElementById('myNav').style.width = '100%';
+    document.getElementById('myNav').style.height = '100%';
   };
 
   const closeNav = () => {
-    document.getElementById("myNav").style.width = "0%";
-    document.getElementById("myNav").style.height = "0%";
+    document.getElementById('myNav').style.width = '0%';
+    document.getElementById('myNav').style.height = '0%';
   };
 
   const handleNavigation = (page) => {
@@ -30,10 +37,11 @@ const App = () => {
   };
 
   const handleResumeClick = () => {
-    const link = document.createElement("a");
-    const resumeUrl = "https://drive.google.com/file/d/10lHtkWE_KsGnLE5_sXslKZBhMdJLtjUl/view?usp=drivesdk";
+    const link = document.createElement('a');
+    const resumeUrl =
+      'https://drive.google.com/file/d/10lHtkWE_KsGnLE5_sXslKZBhMdJLtjUl/view?usp=drivesdk';
     link.href = resumeUrl;
-    link.download = "resume.pdf";
+    link.download = 'resume.pdf';
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
@@ -43,23 +51,82 @@ const App = () => {
     <Router>
       <div>
         <div id="myNav" className="overlay footer-basic_1">
-          <a href="javascript:void(0)" className="closebtn" onClick={closeNav}>&times;</a>
+          <a href="javascript:void(0)" className="closebtn" onClick={closeNav}>
+            &times;
+          </a>
           <div className="overlay-content">
-            <Link to="/" onClick={() => handleNavigation('home')}>Home</Link>
-            <Link to="/PortfolioSection" onClick={() => handleNavigation('PortfolioSection')}>Works</Link>
-            <Link to="/experience" onClick={() => handleNavigation('experience')}>Experience</Link>
-            <Link to="/writing" onClick={() => handleNavigation('writing')}>Writing</Link>
+            <Link
+              to="/"
+              onClick={() => handleNavigation('home')}
+              smooth={true}
+              duration={500}
+              spy={true}
+              exact="true"
+              offset={-80}
+            >
+              Home
+            </Link>
+            <Link
+              to="/PortfolioSection"
+              onClick={() => handleNavigation('PortfolioSection')}
+              smooth={true}
+              duration={500}
+              spy={true}
+              offset={-80}
+            >
+              Works
+            </Link>
+            <Link
+              to="/experience"
+              onClick={() => handleNavigation('experience')}
+              smooth={true}
+              duration={500}
+              spy={true}
+              offset={-80}
+            >
+              Experience
+            </Link>
+            <Link
+              to="/writing"
+              onClick={() => handleNavigation('writing')}
+              smooth={true}
+              duration={500}
+              spy={true}
+              offset={-80}
+            >
+              Writing
+            </Link>
           </div>
           <div className="social">
-            <a href="mailto:ajith24ram@gmail.com" target="_blank" rel="noopener noreferrer"><i className="icon ion-social-google"></i></a>
-            <a href="https://www.linkedin.com/in/ajithkumar-rangappan-a6531a232" target='_blank' rel="noopener noreferrer"><i className="icon ion-social-linkedin"></i></a>
-            <a href="https://github.com/ajith2401" target="_blank" rel="noopener noreferrer"><i className="icon ion-social-github"></i></a>
-            <a href="#"><i className="icon ion-social-instagram"></i></a>
-            <a href="https://www.facebook.com/putinakkavi" target="_blank" rel="noopener noreferrer"><i className="icon ion-social-facebook"></i></a>
+            <a href="mailto:ajith24ram@gmail.com" target="_blank" rel="noopener noreferrer">
+              <i className="icon ion-social-google"></i>
+            </a>
+            <a
+              href="https://www.linkedin.com/in/ajithkumar-rangappan-a6531a232"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <i className="icon ion-social-linkedin"></i>
+            </a>
+            <a href="https://github.com/ajith2401" target="_blank" rel="noopener noreferrer">
+              <i className="icon ion-social-github"></i>
+            </a>
+            <a href="#">
+              <i className="icon ion-social-instagram"></i>
+            </a>
+            <a
+              href="https://www.facebook.com/putinakkavi"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <i className="icon ion-social-facebook"></i>
+            </a>
           </div>
         </div>
-        <div id='nav-icon'>
-          <span style={{ fontSize: '30px', cursor: 'pointer' }} onClick={openNav}>&#9776;</span>
+        <div id="nav-icon">
+          <span style={{ fontSize: '30px', cursor: 'pointer' }} onClick={openNav}>
+            &#9776;
+          </span>
           <button onClick={handleResumeClick}>Resume</button>
         </div>
 
@@ -84,17 +151,80 @@ const App = () => {
         <div className="footer-basic">
           <footer>
             <div className="social">
-              <a href="mailto:ajith24ram@gmail.com" target="_blank" rel="noopener noreferrer"><i className="icon ion-social-google"></i></a>
-              <a href="https://www.linkedin.com/in/ajithkumar-rangappan-a6531a232" target='_blank' rel="noopener noreferrer"><i className="icon ion-social-linkedin"></i></a>
-              <a href="https://github.com/ajith2401" target="_blank" rel="noopener noreferrer"><i className="icon ion-social-github"></i></a>
-              <a href="#"><i className="icon ion-social-instagram"></i></a>
-              <a href="https://www.facebook.com/putinakkavi" target="_blank" rel="noopener noreferrer"><i className="icon ion-social-facebook"></i></a>
+              <a href="mailto:ajith24ram@gmail.com" target="_blank" rel="noopener noreferrer">
+                <i className="icon ion-social-google"></i>
+              </a>
+              <a
+                href="https://www.linkedin.com/in/ajithkumar-rangappan-a6531a232"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <i className="icon ion-social-linkedin"></i>
+              </a>
+              <a href="https://github.com/ajith2401" target="_blank" rel="noopener noreferrer">
+                <i className="icon ion-social-github"></i>
+              </a>
+              <a href="#">
+                <i className="icon ion-social-instagram"></i>
+              </a>
+              <a
+                href="https://www.facebook.com/putinakkavi"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <i className="icon ion-social-facebook"></i>
+              </a>
             </div>
             <ul className="list-inline">
-              <li><Link to="/" onClick={() => handleNavigation('home')}>Home</Link></li>
-              <li><Link to="/PortfolioSection" onClick={() => handleNavigation('PortfolioSection')}>Works</Link></li>
-              <li><Link to="/experience" onClick={() => handleNavigation('experience')}>Experience</Link></li>
-              <li><Link to="/writing" onClick={() => handleNavigation('writing')}>Writing</Link></li>
+              <li>
+                <Link
+                  to="/"
+                  onClick={() => handleNavigation('home')}
+                  smooth={true}
+                  duration={500}
+                  spy={true}
+                  exact="true"
+                  offset={-80}
+                >
+                  Home
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="/PortfolioSection"
+                  onClick={() => handleNavigation('PortfolioSection')}
+                  smooth={true}
+                  duration={500}
+                  spy={true}
+                  offset={-80}
+                >
+                  Works
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="/experience"
+                  onClick={() => handleNavigation('experience')}
+                  smooth={true}
+                  duration={500}
+                  spy={true}
+                  offset={-80}
+                >
+                  Experience
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="/writing"
+                  onClick={() => handleNavigation('writing')}
+                  smooth={true}
+                  duration={500}
+                  spy={true}
+                  offset={-80}
+                >
+                  Writing
+                </Link>
+              </li>
             </ul>
             <p className="text-center">Ajithkumar &copy; 2023</p>
           </footer>
